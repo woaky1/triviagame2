d3c714b39ba0384e4d9ee3dc3114e389c66a364c
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-09-13 09:50:59 -0500

    Got a question to populate via referencing an object.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,11 +4,20 @@ let time = 120;
 var radioValue;
 var graded = false;
 var userAnswers = [];
-var correctAnswers = []
 var numAnsweredCorrect = 0;
 var numAnsweredIncorrect = 0;
 var unansweredQuestions = 0;
 var numberOfQuestions = 10;
+var questionData = [
+        question1 = {
+            question:"What school of magic does the spell Burning Hands belong to?",
+            option1:["Necromancy","incorrect"],
+            option2:["Evocation","correct"],
+            option3:["Conjuration","incorrect"],
+            option4:["Enchantment","incorrect"]
+        }
+]
+var QuestionIndex = 0;
 
 $(document).ready(function(){
     $("#start").on("click", function(){
@@ -32,7 +41,23 @@ function decrement() {
         grade();
     }
 }
+function displayQuestion(i) {
+    $("#questionText").html(questionData[i].question);
+    $("#option1").html(questionData[i].option1[0]);
+    $("#option1").attr("val", questionData[i].option1[1]);
+    $("#option2").html(questionData[i].option2[0]);
+    $("#option2").attr("val", questionData[i].option2[1]);
+    $("#option3").html(questionData[i].option3[0]);
+    $("#option3").attr("val", questionData[i].option3[1]);
+    $("#option4").html(questionData[i].option4[0]);
+    $("#option4").attr("val", questionData[i].option4[1]);
 
+    console.log(questionData[i].option1[0]);
+    i++;
+}
+displayQuestion(QuestionIndex);
+
+// $("#option1"||"#option2"||"#option3"||"#option4").on("click", )
 function grade() {
 // Learned how to loop through and grab the checked button values from one of the answers here: https://stackoverflow.com/questions/45848618/how-to-check-the-values-of-multiple-dynamic-radio-buttons-in-a-form-using-jquery
     
